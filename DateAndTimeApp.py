f16abbf08dcc0ad823a8ee95b2c11872b75f6ff5
anjali515 <anjali15may@gmail.com>
2020-08-05 23:27:35 -0500

    DataAndTimeApp First Commit

--- DateAndTimeApp.py
+++ DateAndTimeApp.py
@@ -1,4 +1,4 @@
-#Python GUI App to print current Date & Time
+#Python GUI App to print Today's Date, Time & Day
 from tkinter import *
 from tkinter import ttk
 import datetime
@@ -9,6 +9,21 @@ def LogCurTime():
     Createstatus.set(now.strftime("%Y-%m-%d %H:%M:%S"))
     return
 
+def GetWeekDayName():
+    # weekdays as a tuple
+    weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    current_time = datetime.datetime.now()
+    todayDate    = datetime.date(current_time.year,current_time.month,current_time.day)
+    todayDay = todayDate.weekday()
+    todayDayAsString = weekDays[todayDay]
+    Weekdayname.set(todayDayAsString)
+    return
+
+def PrintToday():
+    LogCurTime()
+    GetWeekDayName()
+    return 
+
 root = Tk()
 root.geometry("400x100+100+100")
 root.title("Python Current Date And Time GUI App")
@@ -18,10 +33,17 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-ttk.Label(mainframe, text="Current date and time :").grid(column=1, row=1, sticky=W)
+ttk.Label(mainframe, text="Today's date and time :").grid(column=1, row=1, sticky=W)
+
+ttk.Label(mainframe, text="Today's Day :").grid(column=1, row=2, sticky=W)
+
 Createstatus = StringVar()
 ttk.Label(mainframe, textvariable=Createstatus).grid(column=2, row=1, sticky=(W, E))
-b2 = ttk.Button(mainframe, text="ClickToPrint", command=LogCurTime).grid(column=1, row=2, sticky=W)
+
+Weekdayname = StringVar()
+ttk.Label(mainframe, textvariable=Weekdayname).grid(column=2, row=2, sticky=(W, E))
+
+b2 = ttk.Button(mainframe, text="ClickToPrint", command=PrintToday).grid(column=1, row=3, sticky=W)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
